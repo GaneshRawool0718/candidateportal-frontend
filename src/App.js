4215c28562519f6ecf8d915f11ed7d3933609405
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-09 23:38:17 +0530

    feat: set routing for some pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-do
 import SignupForm from './components/SignupForm/SignupForm';
 import LoginForm from './components/LoginForm/LoginForm';
 import DashboardPage from './pages/Dashboard/DashboardPage';
+import CandidateListPage from './components/CandidateList/CandidateListPage';
+import CandidateProfilePage from './components/CandidateProfile/CandidateProfileCard';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Route path="/login" element={<LoginForm />} />
       <Route path="/signup" element={<SignupForm />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/candidates/:status" element={<CandidateListPage />} />
+      <Route path="/candidate/:id" element={<CandidateProfilePage />} /> 
     </Routes>
     </Router>
   );
