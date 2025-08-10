dfffc5ee9b33f2af9fe821da7292bfb327208212
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-10 12:09:04 +0530

    feat: add required constant for apiservices

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,3 +1,4 @@
 // src/constants/apiConstants.js
 
-export const BASE_URL = "http://localhost:8080/auth";
+export const AUTH_BASE_URL = process.env.REACT_APP_AUTH_URL || "http://localhost:8080/auth";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
