13c2b2e98badad6ba1f3504a5248b7714fa1699f
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-10 15:38:20 +0530

    feat:Add utility functions to manage authentication data in localStorage

--- src/utils/storage.jsx
+++ src/utils/storage.jsx
@@ -1,9 +1,20 @@
+// utils/storage.js
 
 export const saveAuthData = (token, user) => {
-    /*
-    Function to save authentication data in local storage.
-    It stores the token and user information for later use in the application.
-    */
   localStorage.setItem('token', token);
   localStorage.setItem('user', JSON.stringify(user));
+};
+
+export const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+export const getUser = () => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+export const clearAuthData = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
 };
